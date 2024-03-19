01ee717207bdd842f4fb6a6032342af58e48e1d2
rubek878 <117920389+rubek878@users.noreply.github.com>
2024-03-19 17:48:21 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function Zalacznik1() {
+  document.getElementById("tekst").innerHTML = "tekstaaaaaaaaaaaaaaaaaaaa";
+}
+function Zalacznik2() {
+  document.getElementById("tekst").innerHTML = "tekstaaaaaaaaaaaaaaaaaaaa";
+}
+function Zalacznik3() {
+  document.getElementById("tekst").innerHTML = "tekstaaaaaaaaaaaaaaaaaaaa";
+}
+function Zalacznik4() {
+  document.getElementById("tekst").innerHTML = "tekstaaaaaaaaaaaaaaaaaaaa";
+}
+function Zalacznik5() {
+  document.getElementById("tekst").innerHTML = "tekstaaaaaaaaaaaaaaaaaaaa";
+}
